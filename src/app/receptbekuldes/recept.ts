14ec4fd65e4fcb84abeac4fcd20ef8859a764a44
aarver <bacsajozseff@gmail.com>
2023-02-12 16:14:39 +0100

    Módosítgatások

--- src/app/receptbekuldes/recept.ts
+++ src/app/receptbekuldes/recept.ts
@@ -3,16 +3,30 @@ export class Recept {
     public id?: string = '';
     public receptNev: string = '';
     public kategoria: string = '';
+    public konyha: string = '';
+    public adag: string = '';
+    public nehezseg: string ='';
+    public koltseg: string ='';
+
     public elfogadottRecept: boolean = false;
     public elokeszulesiIdo: number = 0;
     public elkeszitesiIdo: number = 0;
-    public sutesiHofok: number = 0;
-    public sutesiMod: string = '';
     public sutesiIdo: number = 0;
+    public sutesiMod: string = '';  
+
+
+    public sutesiHofok: number = 0;
+    
     public glutenmentes: boolean = false;
-    public nehezseg: string ='';
+    public cukrotTartalmaz: boolean = false;
+    public tojasmentes: boolean = false;
+    public laktozmentes: boolean = false;
+    public tejmentes: boolean = false;
+
+
+
     public date: string = '';
     public hozzavalo: string = '';
     public elkeszites: string = '';
-    public cukrotTartalmaz: boolean = false;
+    
 }
